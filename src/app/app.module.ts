037b4f91c04a348fd91e28d71bab39aa9bdb4a7b
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-10-11 18:00:49 -0500

    agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { MenuSelectComponent } from './components/menu-select/menu-select.component';
+import { DetailMenuComponent } from './components/detail-menu/detail-menu.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { TotalComponent } from './components/total/total.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    MenuSelectComponent,
+    DetailMenuComponent,
+    OrdersComponent,
+    TotalComponent
   ],
   imports: [
     BrowserModule
