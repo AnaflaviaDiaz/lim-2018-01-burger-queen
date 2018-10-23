cfb20e4b6774138e6ef22f1c9a73bebdea754e8a
anaflaviadiaz <anaflaviadiazmartel5a@gmail.com>
2018-10-23 15:12:23 -0500

    reset init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { MenuSelectComponent } from './components/menu-select/menu-select.compon
 import { DetailMenuComponent } from './components/detail-menu/detail-menu.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { TotalComponent } from './components/total/total.component';
-// import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment.prod';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
 import {FormsModule} from '@angular/forms';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +24,6 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFirestoreModule.enablePersistence(),
